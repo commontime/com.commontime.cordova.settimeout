a34384a5bb9c049cbbf33b468de7a70bd4153d6b
Graham Mackenzie <gmackenzie@commontime.com>
2018-12-11 13:00:01 +0000

    Update setTimeout.js

--- www/setTimeout.js
+++ www/setTimeout.js
@@ -43,7 +43,7 @@ exports.clearInterval = function (id) {
 
 document.addEventListener("deviceready", function() {
     if( window.device.platform === "Android" ) {
-        if( window.device.version.split(".")[0] > 6 ) {
+        if( window.device.version.split(".")[0] > 5 ) {
             window.setTimeout = cordova.plugins.setTimeout.setTimeout;
             window.clearTimeout = cordova.plugins.setTimeout.clearTimeout;
             window.setInterval = cordova.plugins.setTimeout.setInterval;
