3fd0ab9b365b72edbe2e3461e57a01ff7a14f459
Graham Mackenzie <gmackenzie@commontime.com>
2018-12-17 12:03:23 +0000

    Update setTimeout.java

--- src/android/setTimeout.java
+++ src/android/setTimeout.java
@@ -9,28 +9,61 @@ import org.json.JSONObject;
 
 import android.os.SystemClock;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class setTimeout extends CordovaPlugin {
 
+    Timer timer = new Timer();
+    Map<Integer, TimerTask> tasks = new HashMap<>();
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("setTimeout")) {
             int time = args.getInt(0);
-            this.doTimeout(time, callbackContext);
+            int timerId = args.getInt(1);
+            TimerTask timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    callbackContext.success();
+                }
+            };
+            tasks.put(timerId, timerTask);
+            timer.schedule(timerTask, time);
+
+            return true;
+        }
+        if (action.equals("setInterval")) {
+            int time = args.getInt(0);
+            int timerId = args.getInt(1);
+            TimerTask timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    callbackContext.success();
+                }
+            };
+            tasks.put(timerId, timerTask);
+            timer.schedule(timerTask, 0, time);
+
+            return true;
+        }
+        if (action.equals("clearTimeout")) {
+            int timerId = args.getInt(1);
+            tasks.remove(timerId).cancel();
+            return true;
+        }
+        if (action.equals("clearInterval")) {
+            int timerId = args.getInt(1);
+            tasks.remove(timerId).cancel();
             return true;
         }
         return false;
     }
-
-    private void doTimeout(final int time, final CallbackContext callbackContext) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                SystemClock.sleep(time);    
-                callbackContext.success();
-            }
-        }).start();
-    }
 }
+
